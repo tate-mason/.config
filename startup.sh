738cbced5398544e81b7547d90036d6742761750
HistoricalFool <tatem8499@gmail.com>
2024-11-15 15:53:02 -0500

    shell was buggy

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Creating the first window called HomeBase
-tmux new-session -d -s HomeBase
-
-# Create the first pane with NVIM
-tmux send-keys -t HomeBase "nvim" C-m
-
-tmux attach-session -t HomeBase
