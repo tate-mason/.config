62010daf617f6fa57b36fd572d0a15ff03d6bde6
HistoricalFool <tatem8499@gmail.com>
2024-11-15 15:45:46 -0500

    simplifying

--- startup.sh
+++ startup.sh
@@ -6,7 +6,4 @@ tmux new-session -d -s HomeBase
 # Create the first pane with NVIM
 tmux send-keys -t HomeBase "nvim" C-m
 
-# Split that window vertically and send a spotify command to the new pane
-tmux split-window -v -t HomeBase "spotify_player" C-m
-
 tmux attach-session -t HomeBase
