a0513c5f08e25b5610bde1459ba99fb73e924305
HistoricalFool <tatem8499@gmail.com>
2024-11-15 15:15:46 -0500

    Fuck sketchybar

--- sketchybar/items/front_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item front_app left \
-           --set front_app       background.color=$ITEM_BG_COLOR \
-                                 icon.color=$WHITE \
-                                 icon.font="SF Pro:Regular:16.0" \
-                                 label.color=$WHITE \
-                                 script="$PLUGIN_DIR/front_app.sh" \
-           --subscribe front_app front_app_switched
