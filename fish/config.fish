a8384c4c01af49d6a4f25c92be9def7ad146ec66
HistoricalFool <tatem8499@gmail.com>
2025-02-01 13:28:14 -0500

    la;djsk

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set --global --export HOMEBREW_PREFIX "/opt/homebrew";
+set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
+fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
+if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
 
+fisher_add_path $path "~/.juliaup/bin"
 eval "$(/opt/homebrew/bin/brew shellenv)"
-set --global --export HOMEBREW_PREFIX "/opt/homebrew";
-set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
-fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
-if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
-if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
-set --global --export HOMEBREW_PREFIX "/opt/homebrew";
-set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
-fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
-if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
-if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
+alias ls="colorls"
+alias sp="spotify_player"
+fastfetch
+
