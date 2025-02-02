0f3a368a564cf35ece83135e48efdde1d61ca11e
HistoricalFool <tatem8499@gmail.com>
2025-02-02 12:13:58 -0500

    ;alsdkjf

--- nvim/lua/neotex/plugins/snacks.lua
+++ nvim/lua/neotex/plugins/snacks.lua
@@ -8,7 +8,7 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = { enabled = false},
     indent = { enabled = true },
     input = { enabled = true },
     picker = { enabled = true },
