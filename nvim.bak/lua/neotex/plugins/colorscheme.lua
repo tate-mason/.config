6c1ca9d088e25d323eea54c331f8e7d35a405bb1
HistoricalFool <tatem8499@gmail.com>
2025-04-24 20:48:41 -0400

    trying less bloated config

--- nvim.bak/lua/neotex/plugins/colorscheme.lua
+++ nvim.bak/lua/neotex/plugins/colorscheme.lua
@@ -1,28 +1,27 @@
 -- GRUVBOX
-return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require("gruvbox").setup({
-      overrides = {
-        -- THIS BLOCK
-        SignColumn = { bg = "#282828" },
-        NvimTreeCutHL = { fg = "#fb4934", bg = "#282828" },
-        NvimTreeCopiedHL = { fg = "#b8bb26", bg = "#282828" },
-        DiagnosticSignError = { fg = "#fb4934", bg = "#282828" },
-        DiagnosticSignWarn = { fg = "#fabd2f", bg = "#282828" },
-        DiagnosticSignHint = { fg = "#8ec07c", bg = "#282828" },
-        DiagnosticSignInfo = { fg = "#d3869b", bg = "#282828" },
-        -- OR THIS BLOCK
-        -- NvimTreeCutHL = { fg="#fb4934", bg="#3c3836" },
+--return {
+--  "ellisonleao/gruvbox.nvim",
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--  config = function()
+--    require("gruvbox").setup({
+--      overrides = {
+--        -- THIS BLOCK
+--        SignColumn = { bg = "#282828" },
+--        NvimTreeCutHL = { fg = "#fb4934", bg = "#282828" },
+--        NvimTreeCopiedHL = { fg = "#b8bb26", bg = "#282828" },
+--        DiagnosticSignError = { fg = "#fb4934", bg = "#282828" },
+--        DiagnosticSignWarn = { fg = "#fabd2f", bg = "#282828" },
+--        DiagnosticSignHint = { fg = "#8ec07c", bg = "#282828" },
+--        DiagnosticSignInfo = { fg = "#d3869b", bg = "#282828" },
+--        -- OR THIS BLOCK        -- NvimTreeCutHL = { fg="#fb4934", bg="#3c3836" },
         -- NvimTreeCopiedHL = { fg="#b8bb26", bg="#3c3836" }
         -- END
-      }
-    })
-    vim.cmd("colorscheme gruvbox")
-  end,
-}
-
+      --}
+    --})
+    --vim.cmd("colorscheme gruvbox")
+  --end,
+--}
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 -- -- KANAGAWA
 -- return {
 --   "rebelot/kanagawa.nvim",
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,2 @@
-require("neotex.core")
-require("neotex.bootstrap")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
