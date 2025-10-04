36597373f155596e2627bd047b97de15735ba5a6
tate-mason <tatem8499@gmail.com>
2025-10-04 11:17:17 -0400

    lalal

--- nvim/lua/plugins/stata.lua
+++ nvim/lua/plugins/stata.lua
@@ -1,3 +1,30 @@
 return {
-  "poliquin/stata-vim",
+  "human-d3v/stata-nvim",
+  branch = "packaging",
+  ft = { "stata" },
+  build = "git pull origin packaging && cd lsp-server && npm init -y && npm install && bun build ./server/src/server.ts --compile --outfile server_bin && cd ..",
+  opts = {
+    dev = false,
+    stata_license_type = "stata-mp",
+  },
+  config = function(_, opts)
+    local stata = require("stata-nvim")
+    stata.setup(opts)
+
+    vim.filetype.add({
+      extension = {
+        ["do"] = "stata",
+        ado = "stata",
+      },
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "stata",
+      callback = function(ev)
+        vim.bo[ev.buf].expandtab = true
+        vim.bo[ev.buf].shiftwidth = 2
+        vim.bo[ev.buf].tabstop = 2
+      end,
+    })
+  end,
 }
