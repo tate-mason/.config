c36ba54ae4fedaa7d0c7fc0be47dc1599d47457a
tate-mason <tatem8499@gmail.com>
2026-06-25 10:39:24 -0400

    la

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 -- ===== Theme Cycling Setup =====
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      variant = "wave", -- "wave", "dragon", or "lotus"
-      transparent = true, -- set true if you want terminal bg
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "kanagawa-wave",
-    },
-  },
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "latte",
+      integrations = {
+        telescope = true,
+        treesitter = true,
+        lualine = true,
+      },
+    })
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
